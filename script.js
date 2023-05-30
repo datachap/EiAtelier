409a71348b14f68ad2fff6a7c1fe3b2025ca8f46
iameugn <metajjuxhin@gmail.com>
2023-05-30 16:12:46 +0200

    Smooth scroll added

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-// Smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-  
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+ // Smooth scroll function
+ function smoothScroll(target, duration) {
+    var targetElement = document.querySelector(target);
+    var targetPosition = targetElement.offsetTop;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+
+    function scrollAnimation(currentTime) {
+      if (startTime === null) startTime = currentTime;
+      var timeElapsed = currentTime - startTime;
+      var run = ease(timeElapsed, startPosition, distance, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(scrollAnimation);
+    }
+
+    // Easing function
+    function ease(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return c / 2 * t * t + b;
+      t--;
+      return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(scrollAnimation);
+  }
+
+  // Smooth scroll when clicking on navigation links
+  var navLinks = document.querySelectorAll('nav ul li a');
+  navLinks.forEach(function(link) {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+      var target = this.getAttribute('href');
+      smoothScroll(target, 800);
     });
   });
-  
   document.addEventListener("DOMContentLoaded", function() {
     $('.gallery-carousel').slick({
         autoplay:true,
