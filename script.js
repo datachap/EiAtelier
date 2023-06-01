3078476de0510264b226d3aea35030436faf07ca
iameugn <metajjuxhin@gmail.com>
2023-06-01 23:52:28 +0200

    Requested changes added

--- script.js
+++ script.js
@@ -63,36 +63,13 @@
 
 
  var main = new Splide('#gallery', {
-   type: 'carousel',
+   type: 'loop',
+   padding: '5rem',
    heightRatio: 0.5,
    pagination: true,
    arrows: false,
    cover: true,
-   autoplay: true,
  });
 
- var thumbnails = new Splide('#thumbnail-slider', {
-   type: 'slide',
-   rewind: true,
-   fixedWidth: 104,
-   fixedHeight: 58,
-   isNavigation: true,
-   gap: 10,
-   focus: 'center',
-   pagination: false,
-   cover: true,
-   dragMinThreshold: {
-     mouse: 4,
-     touch: 10,
-   },
-   breakpoints: {
-     640: {
-       fixedWidth: 66,
-       fixedHeight: 38,
-     },
-   },
- });
-
- main.sync(thumbnails);
+ 
  main.mount();
- thumbnails.mount();
